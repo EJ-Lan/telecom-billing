7622d0dc1e85cec3593649836c8305731809e8d6
Ervin Lanada <ervinjon_lanada@outlook.com>
2025-11-11 20:55:06 -0700

    feat: add record function

--- billing.c
+++ billing.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 // Structure to hold customer information
 struct Customer {
   char name[50];
@@ -11,3 +14,23 @@ struct Customer customers[100];
 
 // Global variable to keep track of the number of customers
 int customerCount = 0;
+
+// Function to add a new customer record
+void addRecord()
+{
+  if (customerCount < 100) {
+    printf("\nEnter name: ");
+    scanf(" %[^\n]s", customers[customerCount].name);
+    printf("Enter phone number: ");
+    scanf("%s", customers[customerCount].phoneNumber);
+    printf("Enter usage (in minutes): ");
+    scanf("%f", &customers[customerCount].usage);
+    customers[customerCount].totalBill
+      = customers[customerCount].usage * 0.1;
+    customerCount++;
+    printf("\nRecord added successfully!\n");
+  }
+  else {
+    printf("\nMaximum number of records reached!\n");
+  }
+}
